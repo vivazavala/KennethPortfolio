aa0d11708439ef10911121d4266df7acf0806432
vivazavala1 <kennethz180@yahoo.com>
2022-04-01 02:34:26 -0500

    Added homepage rough draft

--- kennethportfolio/src/Components/Home.js
+++ kennethportfolio/src/Components/Home.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import {Row} from "react-bootstrap";
-import {motion} from "framer-motion";
+import {React, useContext} from 'react';
+import {Row, Container, Col} from "react-bootstrap";
+import {motion, useSpring} from "framer-motion";
+import './Home.css';
+import TypeWriter from 'typewriter-effect';
+import {ViewSize} from "../App.js";
+
+
+
+
 function Home() {
+  const PageSize = useContext(ViewSize);
+  const variants ={
+
+  }
   return (
     <motion.div 
     initial={{scaleY: 0}} 
@@ -9,7 +20,26 @@ function Home() {
     exit={{scaleY:0}}
     transition={{duration: .5, delay: 1}}
     >
-    <div>Hello world you are home</div>
+    
+    <Container fluid>
+      <ul >
+      <motion.li className={PageSize.width >= 500 ? "li" : "phoneli"} whileHover={{color: '#c227e6'}}>
+      Hi, <br></br>
+      I'm Kenneth, 
+      </motion.li>
+      <motion.li className ={PageSize.width >= 500 ? "typeWriterText" : "phonetypeWriterText"} whileHover={{color:'#c227e6'}}>
+         <TypeWriter options={{
+          strings: ['Software Engineer', 'Full-Stack Engineer'],
+          autoStart: true,
+          loop: true,
+          delay: 65, deleteSpeed: 60, pauseFor: 1000
+           }} />
+      </motion.li>
+      <motion.button className='button' whileTap={{scale: .5}} whileHover={{scale: 1.1}}>Contact Me!</motion.button>
+      <motion.button className='button' whileTap={{scale: .5}} whileHover={{scale: 1.1}}>View My Work!</motion.button>
+      
+      </ul>
+    </Container>
     </motion.div>
   );
 }
--- kennethportfolio/src/Components/NavBar.js
+++ kennethportfolio/src/Components/NavBar.js
@@ -1,15 +1,19 @@
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useLocation} from 'react-router-dom';
 import { React, useState, useEffect, useContext} from "react";
 import {SocialMediaIconsReact} from 'social-media-icons-react';
-import { Navbar, Nav, Container, Row, Col, Brand} from 'react-bootstrap';
+import { Navbar, Nav, Container, Row, Col} from 'react-bootstrap';
 import cvicon from "../images/cvicon.png";
+import kzlogo from "../images/KZ.png";
 import Pdf from "../documents/ZavalaKennethResume2022pdfWeb.pdf";
 import {ViewSize} from "../App.js";
 import {motion} from "framer-motion";
 
 var navLinkStyle ={
-    fontSize: '20px'   
+    fontSize: '20px'
+}
+var hamburgerLinkStyle ={
+    width: '15%'
 }
 var navBarCustomCol = {
     backgroundColor: '#000000'
@@ -22,29 +26,45 @@ var iconStyle ={
     paddingRight: "5px"
 }
 var phoneIconStyle={
-    paddingRight: "20px",
+    paddingLeft:'13px',
+    paddingRight: "13px",
     justifyContent: 'center',
     display: 'flex'
 }
 var phoneNav ={
     fontSize: '15px',
-    padding: '2px',
     transition: '0.3s all ease-out'
 }
 var phoneLinkStyle={  
     transition: '0.2s all ease-out',
     display: 'flex',
-    fontSize: '15px',
-    justifyContent: 'center'
+    fontSize: '18px',
+    justifyContent: 'center',
+    padding: '10px',
 }
+var logoStyle = {
+    height: '55px',
+    width: '70px'
+}
+var phoneLogoStyle = {
+    height: '30px',
+    width: '45px'
+}
+
 
 
 function NavBar() {
        const PageSize = useContext(ViewSize);
-        const variants={
+       const location = useLocation();
+         const variants = {
             visible: {opacity:1},
             hidden:{opacity: 0},
         }
+
+function onEmailClick(){
+window.open('mailto:kennethz180@yahoo.com');
+}
+
 	return (
         <motion.div 
 initial="hidden" 
@@ -55,26 +75,28 @@ transition={{ease: "easeInOut", duration: 1}}
         <div>
             <Navbar collapseOnSelect expand="lg" fluid style={navBarCustomCol} variant="dark">
             <Container fluid>
-           
+
                 <Nav className='ms-auto'>
-                    <Navbar.Brand href="/"style ={PageSize.width >= 700 ? navLinkStyle : phoneNav}>KENNETH ZAVALA</Navbar.Brand>
+                <motion.div whileHover={{scale: 1.1}}>
+                    <Navbar.Brand href="/" title ="Home" style ={PageSize.width >= 700 ? navLinkStyle : phoneNav}><img src = {kzlogo} style ={PageSize.width >= 500 ? logoStyle : phoneLogoStyle}/></Navbar.Brand>
+                </motion.div>   
                 </Nav>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">   
 
-                <Nav className="m-auto" style= {navLinkStyle}>                  
+                <Nav className="m-auto" style= {PageSize.width >= 991 ?  navLinkStyle : hamburgerLinkStyle} activeKey ={location.pathname}>                  
                     <Nav.Link href="/About" style={PageSize.width >= 991 ? linkStyle : phoneLinkStyle}>About</Nav.Link>
                     <Nav.Link href="/Portfolio" style={PageSize.width >= 991 ? linkStyle : phoneLinkStyle}>Portfolio</Nav.Link>
                     <Nav.Link href="/Contact" style={PageSize.width >= 991 ? linkStyle : phoneLinkStyle}>Contact</Nav.Link>                  
                 </Nav>
-           
+                
                 <Nav className={PageSize.width >= 991 ? "m1-auto" : "flex-row justify-content-center"}>     
-                    <Nav.Link href="https://www.linkedin.com/in/kennethzavala/" style={PageSize.width >= 991 ? iconStyle : phoneIconStyle} title="LinkedIn"><SocialMediaIconsReact  borderColor="#FFFFFF" borderWidth="3" borderStyle="solid" icon="linkedin" iconColor="#000000" backgroundColor="#FFFFFF" iconSize="3" roundness="50%" url="https://www.linkedin.com/in/kennethzavala/" size={PageSize.width >= 991 ? "50": "30"} /></Nav.Link>
-                    <Nav.Link href ="https://github.com/vivazavala" style= {PageSize.width >= 991 ? iconStyle : phoneIconStyle} title="github"><SocialMediaIconsReact  borderColor="#000000" borderWidth="0" borderStyle="solid" icon="github" iconColor="#000000" backgroundColor="#FFFFFF" iconSize="8" roundness="50%" url="https://github.com/vivazavala" size={PageSize.width >= 991 ? "50": "30"} /></Nav.Link>
-                    <Nav.Link href={Pdf} style={PageSize.width >= 991 ? iconStyle : phoneIconStyle} title="View Resume">
+                    <motion.div whileHover={{scale: 1.1}}><Nav.Link href="https://www.linkedin.com/in/kennethzavala/" style={PageSize.width >= 991 ? iconStyle : phoneIconStyle} title="LinkedIn"><SocialMediaIconsReact  borderColor="#FFFFFF" borderWidth="3" borderStyle="solid" icon="linkedin" iconColor="#000000" backgroundColor="#FFFFFF" iconSize="3" roundness="50%" url="https://www.linkedin.com/in/kennethzavala/" size={PageSize.width >= 991 ? "50": "30"} /></Nav.Link></motion.div>
+                    <motion.div whileHover={{scale: 1.1}}><Nav.Link href ="https://github.com/vivazavala" style= {PageSize.width >= 991 ? iconStyle : phoneIconStyle} title="github"><SocialMediaIconsReact borderColor="#000000" borderWidth="0" borderStyle="solid" icon="github" iconColor="#000000" backgroundColor="#FFFFFF" iconSize="8" roundness="50%" url="https://github.com/vivazavala" size={PageSize.width >= 991 ? "50": "30"} /></Nav.Link></motion.div>
+                    <motion.div whileHover={{scale: 1.1}}><Nav.Link href={Pdf} style={PageSize.width >= 991 ? iconStyle : phoneIconStyle} title="View Resume">
                     <img src={cvicon} style={{borderRadius: "50%"}} alt="Resume" width={PageSize.width >= 991 ? "50": "30"} height ={PageSize.width >= 991 ? "50": "30"} size="50"/>
-                   </Nav.Link>
-                   <Nav.Link eventKey={2} href ="#Contact" style={PageSize.width >= 991 ? iconStyle : phoneIconStyle} title="Email Me"><SocialMediaIconsReact  borderColor="#000000" borderWidth="0" borderStyle="solid" icon="mail" iconColor="#000000" backgroundColor="#FFFFFF" iconSize="6" roundness="50%" size={PageSize.width >= 991 ? "50": "30"} /></Nav.Link>
+                   </Nav.Link></motion.div>
+                   <motion.div whileHover={{scale: 1.1}}><Nav.Link eventKey={2} href ="/Contact" onClick ={onEmailClick} style={PageSize.width >= 991 ? iconStyle : phoneIconStyle} title="Email Me"><SocialMediaIconsReact  borderColor="#000000" borderWidth="0" borderStyle="solid" icon="mail" iconColor="#000000" backgroundColor="#FFFFFF" iconSize="6" roundness="50%" size={PageSize.width >= 991 ? "50": "30"} /></Nav.Link></motion.div>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
